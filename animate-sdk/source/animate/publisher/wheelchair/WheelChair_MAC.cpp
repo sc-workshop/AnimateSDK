5f059d9537880b19f7716f39cb35fac91caa4071
Daniil-SV <daniilsivacenko@gmail.com>
2026-02-17 20:24:29 +0200

    updated wheelchair offsets for apple silicon build

--- animate-sdk/source/animate/publisher/wheelchair/WheelChair_MAC.cpp
+++ animate-sdk/source/animate/publisher/wheelchair/WheelChair_MAC.cpp
@@ -7,6 +7,20 @@ namespace Animate
 	{
 		size_t major = ((version >> 24) & 0xFF);
 
+		#if defined(__aarch64__)
+        if (major == 24)
+        {
+            CPicPage_Is9SliceEnabled = 382;
+            CPicPage_9SliceGuides = 1532;
+
+            CPicObj_IsButton = 552;
+            CPicSprite_GetName = 912;
+
+            m_initialized = true;
+            return;
+        }
+        
+		#else
 		// Animate 2024
 		if (major == 24)
 		{
@@ -30,6 +44,7 @@ namespace Animate
 			m_initialized = true;
 			return;
 		}
+		#endif
 
 		m_initialized = false;
 	}
